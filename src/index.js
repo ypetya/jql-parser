e5ef4c222beba8d04e052226471bcf6de7a706ca
Peter Kiss <ypetya@gmail.com>
2020-01-11 22:56:02 +0100

    Remove js files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Sentence_1 = require("./language/Sentence");
-exports.parse = function (str) {
-    var s = new Sentence_1.Sentence();
-    return s.parse(str);
-};
